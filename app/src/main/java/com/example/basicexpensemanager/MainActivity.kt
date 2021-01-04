4ed294df0026e6942c4e43ed28796e0c096911ea
Saumi-Pandya <saumipandya@gmail.com>
2021-01-04 18:48:11 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/basicexpensemanager/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.basicexpensemanager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import java.text.DateFormat
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //var currentdate: Date = Calendar.getInstance().time
+        //var dateStr: String = DateFormat.getInstance().format(currentdate)
+
+
+    }
+}
